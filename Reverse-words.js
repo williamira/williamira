57e3dfa34b17ca054dc9b8e72a4d5adef1737fee
williamira <148262181+williamira@users.noreply.github.com>
2023-11-22 20:51:39 +0000

    Reverse-words

---
+++ Reverse-words.js
@@ -0,0 +1,13 @@
+const reverseWords = function(str) {  
+    let arrSplit = str.split(" ");
+    let strReversed = "";
+            for (let i = arrSplit.length - 1; i >= 0; i--) {
+                strReversed += arrSplit[i] + " ";
+            }
+    return strReversed;
+  };
+  
+  console.log(
+    reverseWords('hello and welcome to Lighthouse Labs'),
+    reverseWords('am I Yoda')
+  );
